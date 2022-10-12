52468d2f459e151da06313e366d01060d1946ca3
Luis Puentes Vega <luispuentesvega@gmail.com>
2022-10-12 15:28:03 +0700

    feat: add getRandom number to helper functions

--- src/session-02/helper.js
+++ src/session-02/helper.js
@@ -1,12 +1,18 @@
-function greet(firstName, lastName) {
+const greet = (firstName, lastName) => {
   return `Hello ${this.getFullName(firstName, lastName)}`;
-}
+};
 
-function getFullName(firstName, lastName) {
+const getFullName = (firstName, lastName) => {
   return `${firstName} ${lastName}`;
-}
+};
+
+const getRandomNumber = () => Math.random();
+
+//TODO: add unit testing to greet() function and mock getFullName fn
+//TODO: add unit to getRandomNumber() function and mock Math fn
 
 module.exports = {
   greet,
   getFullName,
+  getRandomNumber,
 };
