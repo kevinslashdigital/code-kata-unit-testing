3e0581de9ae0392a2f5373c915f9f65b305fe743
Luis Puentes Vega <luispuentesvega@gmail.com>
2022-11-11 19:05:49 +0700

    add skeleton for api-client test

--- src/session-03/api-client.test.js
+++ src/session-03/api-client.test.js
@@ -1,41 +1,22 @@
 const axios = require("axios");
 
-const { getMovies } = require("./api-client");
+const { getMovies, addMovie } = require("./api-client");
 
 jest.mock("axios");
-jest.mock("./helper.js", () => ({
+jest.mock("../session-02/helper.js", () => ({
   getEnvs: jest.fn().mockReturnValue({ API: "url" }),
 }));
 
 describe("# getMovies", () => {
   it("should return users list when axios response is successfully", async () => {
     // arrange
-    const body = {
-      data: [
-        { id: 1, name: "movie 01" },
-        { id: 2, name: "movie 02" },
-      ],
-    };
-    axios.get.mockResolvedValueOnce(body);
-
     // act
-    const result = await getMovies();
-
     // assert
-    expect(axios.get).toHaveBeenCalledWith(`url/api/movies`);
-    expect(result).toEqual(body.data);
   });
 
-  it("should return empty users when there is one error in the endpoint request", async () => {
+  it("should return error  when there is one error in the endpoint request", async () => {
     // arrange
-    const message = "Network Error";
-    axios.get.mockRejectedValueOnce(message);
-
     // act
-    const result = await getMovies();
-
     // assert
-    expect(axios.get).toHaveBeenCalledWith(`url/api/movies`);
-    expect(result).toEqual([]);
   });
 });
