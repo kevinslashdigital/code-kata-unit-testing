8245b230bafceeb44df72a595eba01293c2a86a1
Luis Puentes Vega <luispuentesvega@gmail.com>
2022-11-11 19:38:07 +0700

    add async function

--- src/session-03/async-functions.js
+++ src/session-03/async-functions.js
@@ -5,6 +5,14 @@ const fetch = (url) => {
   });
 };
 
+function addAsync(a, b, callback) {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+}
+
 module.exports = {
   fetch,
+  addAsync,
 };
